7b1a4419825047de5880afa6edb789d5dda27833
AmeenFadel <ameenfadel820@gmail.com>
2024-10-30 15:54:49 +0300

    update

--- src/components/RecentCard/RecentCard.jsx
+++ src/components/RecentCard/RecentCard.jsx
@@ -3,7 +3,7 @@ export default function RecentCard({title,des,photo}) {
     return (
         <div className='RecentCard'>
             <div className='imgBorder'>
-                <img src={photo} alt="" />
+                <img src={photo} alt="photo" />
             </div>
             <h3>{title}</h3>
             <p>{des}</p>
